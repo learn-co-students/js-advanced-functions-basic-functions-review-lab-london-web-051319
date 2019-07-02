e9b28da2a6eceb6e1bd094a1668836383d631dc8
James <hayward.jp@gmail.com>
2019-07-02 12:08:17 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function wrapAdjective(arg = '*') {
+   return function(param = 'special') {
+      return `You are ${arg}${param}${arg}!`;
+   };
+};
+
+function saturdayFun(activity = 'roller-skate') {
+   return `This Saturday, I want to ${activity}!`
+};
+
+function mondayWork(activity = 'go to the office') {
+   return `This Monday, I will ${activity}.`
+};
+
+const Calculator = {
+   add: function(a, b) {return a + b},
+   subtract: function(a, b) {return a - b;},
+   divide: function(a, b) {return a / b},
+   multiply: function(a, b) {return a * b},
+};
+
+
+function actionApplyer(start, array) {
+   let value = start;
+   for(const expression of array){ 
+      value = expression(value);
+   }
+   return value;
+};
+
