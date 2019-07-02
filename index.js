92ed800622a6945c06e5ad857c45331027c78c2a
AlisherZakir <a3zakir@gmail.com>
2019-07-03 00:13:51 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+const saturdayFun = (string = "roller-skate") => `This Saturday, I want to ${string}!`;
+
+const mondayWork = (activity = "go to the office") => `This Monday, I will ${activity}.`;
+
+const wrapAdjective = (wrap = "*") => (string) => `You are ${wrap + string + wrap}!`;
+
+const Calculator = new Object()
+  Calculator.add = (a,b) => a + b;
+  Calculator.subtract = (a,b) => a - b;
+  Calculator.multiply = (a,b) => a * b;
+  Calculator.divide = (a,b) => a / b;
+
+const actionApplyer = (integer, funcArr) => funcArr.reduce((result, func) => func(result), integer);
